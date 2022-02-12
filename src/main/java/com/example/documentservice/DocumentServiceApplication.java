56df6585080d1384259f303591095d91466381e6
Anatoly Obukhov <toll1605@gmail.com>
2022-02-12 17:04:41 +0100

    Dependencies in jar

--- src/main/java/com/example/documentservice/DocumentServiceApplication.java
+++ src/main/java/com/example/documentservice/DocumentServiceApplication.java
@@ -1,5 +1,10 @@
 package com.example.documentservice;
 
+
+import com.thoughtworks.xstream.XStream;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +19,22 @@ public class DocumentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentServiceApplication.class, args);
     }
+
+
+    /**
+     * Correctly configuring the XStream serializer to avoid security warnings.
+     */
+    @Autowired
+    public void configure(Serializer serializer) {
+        if(serializer instanceof XStreamSerializer) {
+            XStream xStream = ((XStreamSerializer)serializer).getXStream();
+            XStream.setupDefaultSecurity(xStream);
+            xStream.allowTypesByWildcard(new String[] { "io.axoniq.demo.**", "org.axonframework.**" });
+        }
+    }
+
+
+
+
 }
 
